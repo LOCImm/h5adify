30420a65a3e8fba5491548579c6f5c135e87d920
LOCImm <155392582+LOCImm@users.noreply.github.com>
2025-12-14 14:26:11 +0100

    Delete src/h5adify directory

--- src/h5adify/__init__.py
+++
@@ -1,7 +0,0 @@
-"""h5adify public API."""
-
-from .highlevel import download, batch_download
-from .merge import merge_h5ads
-
-__all__ = ["download", "batch_download", "merge_h5ads"]
-__version__ = "0.1.0"
